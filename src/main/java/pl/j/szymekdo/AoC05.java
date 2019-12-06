244eb7c379af403d5eeb800961505d94c1df29a5
Szymon Rakowski <szymekdo@gmail.com>
2019-12-06 01:17:50 +0100

    Day 05 solved

--- src/main/java/pl/j/szymekdo/AoC05.java
+++ src/main/java/pl/j/szymekdo/AoC05.java
@@ -11,20 +11,25 @@ public class AoC05 extends AoC {
 
     public AoC05() throws Exception {
         int[] content = Arrays.stream(readFile().split(",")).mapToInt(e -> Integer.parseInt(e.trim())).toArray();
-        int[] input = new int[]{1};
+        int[] input = new int[]{5};
         int currentPosition = 0;
         int inputPosition = 0;
         int opcode;
         outer:
         while ((opcode = content[currentPosition]) != 99) {
             if (opcode < 100) {
+                int parameter1 = content[content[currentPosition + 1]];
+                int parameter2;
+                int parameter3 = content[currentPosition + 3];
                 switch (opcode) {
                     case 1:
-                        content[content[currentPosition + 3]] = content[content[currentPosition + 1]] + content[content[currentPosition + 2]];
+                        parameter2 = content[content[currentPosition + 2]];
+                        content[parameter3] = parameter1 + parameter2;
                         currentPosition += 4;
                         break;
                     case 2:
-                        content[content[currentPosition + 3]] = content[content[currentPosition + 1]] * content[content[currentPosition + 2]];
+                        parameter2 = content[content[currentPosition + 2]];
+                        content[parameter3] = parameter1 * parameter2;
                         currentPosition += 4;
                         break;
                     case 3:
@@ -35,26 +40,62 @@ public class AoC05 extends AoC {
                         System.out.println(content[content[currentPosition + 1]]);
                         currentPosition += 2;
                         break;
+                    case 5:
+                        if (parameter1 != 0) {
+                            parameter2 = content[content[currentPosition + 2]];
+                            currentPosition = parameter2;
+                        }else
+                            currentPosition+=3;
+                        break;
+                    case 6:
+                        if (parameter1 == 0) {
+                            parameter2 = content[content[currentPosition + 2]];
+                            currentPosition = parameter2;
+                        }else
+                            currentPosition+=3;
+                        break;
+                    case 7:
+                        parameter2 = content[content[currentPosition + 2]];
+                        if (parameter1 < parameter2)
+                            content[parameter3] = 1;
+                        else
+                            content[parameter3] = 0;
+                        currentPosition += 4;
+                        break;
+                    case 8:
+                        parameter2 = content[content[currentPosition + 2]];
+                        if (parameter1 == parameter2)
+                            content[parameter3] = 1;
+                        else
+                            content[parameter3] = 0;
+                        currentPosition += 4;
+                        break;
                     default:
                         break outer;
                 }
-            }else {
+            }
+            else {
                 char[] chars = StringUtils.leftPad(String.valueOf(opcode), 5, '0').toCharArray();
                 int[] parameterModes = new int[3];
-                parameterModes[2]=chars[0]-0x30;
-                parameterModes[1]=chars[1]-0x30;
-                parameterModes[0]=chars[2]-0x30;
-                int opcode2 = (chars[3]-0x30)*10+chars[4]-0x30;
-                int parameter3=parameterModes[2]==0?content[content[currentPosition + 3]]:content[currentPosition+3];
-                int parameter1=parameterModes[0]==0?content[content[currentPosition + 1]]:content[currentPosition+1];
-                int parameter2=parameterModes[1]==0?content[content[currentPosition + 2]]:content[currentPosition+2];
+                parameterModes[2] = chars[0] - 0x30;
+                parameterModes[1] = chars[1] - 0x30;
+                parameterModes[0] = chars[2] - 0x30;
+                int opcode2 = (chars[3] - 0x30) * 10 + chars[4] - 0x30;
+                int parameter3 = content[currentPosition + 3];
+
+                int parameter1 = parameterModes[0] == 0 ? content[content[currentPosition + 1]] : content[currentPosition + 1];
+
+                int parameter2;
+
                 switch (opcode2) {
                     case 1:
+                        parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
                         content[parameter3] = parameter1 + parameter2;
                         currentPosition += 4;
                         break;
                     case 2:
-                        content[parameter3]  = parameter1 * parameter2;
+                        parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
+                        content[parameter3] = parameter1 * parameter2;
                         currentPosition += 4;
                         break;
                     case 3:
@@ -62,16 +103,47 @@ public class AoC05 extends AoC {
                         currentPosition += 2;
                         break;
                     case 4:
-                        System.out.println(content[parameter1]);
+                        System.out.println(parameter1);
                         currentPosition += 2;
+                        break;
+                    case 5:
+                        if (parameter1 != 0) {
+                            parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
+                            currentPosition = parameter2;
+                        }else
+                            currentPosition+=3;
+                        break;
+                    case 6:
+                        if (parameter1 == 0) {
+                            parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
+                            currentPosition = parameter2;
+                        }else
+                            currentPosition+=3;
+                        break;
+                    case 7:
+                        parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
+                        if (parameter1 < parameter2)
+                            content[parameter3] = 1;
+                        else
+                            content[parameter3] = 0;
+                        currentPosition += 4;
+                        break;
+                    case 8:
+                        parameter2 = parameterModes[1] == 0 ? content[content[currentPosition + 2]] : content[currentPosition + 2];
+                        if (parameter1 == parameter2)
+                            content[parameter3] = 1;
+                        else
+                            content[parameter3] = 0;
+                        currentPosition += 4;
                         break;
                     default:
                         break outer;
                 }
 
+
             }
         }
-        System.out.println(content[4]);
+        //System.out.println(content[4]);
 
     }
 }
